7a7a534b5bf0c1a0410ff5ad94ffb988b37d4fbe
nageswar8 <81064944+nageswar8@users.noreply.github.com>
2021-12-19 09:35:49 +0530

    Update and rename sonar-project.js to sonar.js

--- sonar.js
+++ sonar.js
@@ -4,9 +4,9 @@ sonarqubeScanner({
   serverUrl: 'http://13.211.116.157:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectName': 'Node JS Application',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'a782a2638134d9267aa7d0d0c4e2266cc7583938',
+	    'sonar.login': '8f663d53264a30eceb4e9032b139a755237f8fc1',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
